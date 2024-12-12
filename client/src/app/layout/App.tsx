8a3aa63d567bd8174bf4ab50141194baa8edd1c1
Angela Alagjozovska <aalagozovska@gmail.com>
2024-12-12 22:56:34 +0100

    End of section 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,33 @@
 import Catalog from "../../features/catalog/Catalog";
-import { Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    }
+    else{
+      setLoading(false);
+    }
+    
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -18,9 +39,11 @@ function App() {
     }
   })
 
-function handleThemeChange() {
-  setDarkMode(!darkMode);
-}
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  if (loading) return <LoadingComponent message="Initialising app..." />
 
   return (
     <ThemeProvider theme={theme}>
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -7,6 +7,8 @@ import AboutPage from "../../features/about/AboutPage";
 import { ContactPage } from "@mui/icons-material";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 
 export const router = createBrowserRouter([
     {
@@ -19,7 +21,8 @@ export const router = createBrowserRouter([
             {path: 'about', element: <AboutPage />},
             {path: 'contact', element: <ContactPage />},
             {path: 'server-error', element: <ServerError />},
-            {path: 'not-found', element: <NotFound />},
+            {path: 'basket', element: <BasketPage />},
+            {path: 'checkout', element: <CheckoutPage />},
             {path: '*', element: <Navigate replace to='/not-found' />},
         ]
     }
